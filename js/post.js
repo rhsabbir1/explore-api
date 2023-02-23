acfa9fc8c199ba95224eb95cb9eaeea033f20a6a
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-23 16:32:16 +0600

    show post

--- js/post.js
+++ js/post.js
@@ -1,11 +1,20 @@
-function loadPost (){
+function loadPost() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => console.log(data))
+        .then(res => res.json())
+        .then(data => displayPost(data))
 }
-function displayPost(data){
-    for(const post of data){
+function displayPost(data) {
+    const postContainer = document.getElementById('post-container');
+    for (const post of data) {
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `
+        <h2>User : ${post.id}</h2>
+        <h3>Title : ${post.title}</h3>
+        <p>Description : ${post.body}</p>
+         `;
         console.log(post)
+        postContainer.appendChild(div)
     }
 }
 
